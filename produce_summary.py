8d655e4e165f1f76641fe86bdcaecfee9008706b
Jennifer Park <zp@Jennifers-MacBook-Air.local>
2021-03-17 22:35:34 -0700

    added text files, fn

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,23 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def print_report(file_name):
+    the_file = open(file_name) #open Day's file
+    for line in the_file: #go through every line in file
+        line = line.rstrip()
+        words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        melon = words[0]
+        count = words[0]
+        amount = words[0]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
 
+print("Day 1") #for day 1 
+print_report(file_name = "um-deliveries-20140519.txt")
 
 print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
+print_report(file_name = "um-deliveries-20140520.txt")
 
 print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+print_report(file_name = "um-deliveries-20140521.txt")
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
