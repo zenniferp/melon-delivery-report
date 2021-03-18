c85def725b71e42eb3e8b606fafa46df49ce3841
Jennifer Park <zp@Jennifers-MacBook-Air.local>
2021-03-17 22:11:54 -0700

    Initial Commit

---
+++ produce_summary.py
@@ -0,0 +1,43 @@
+print("Day 1")
+the_file = open("um-deliveries-20140519.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+
+    print("Delivered {} {}s for total of ${}".format(
+        count, melon, amount))
+the_file.close()
+
+
+print("Day 2")
+the_file = open("um-deliveries-20140520.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+
+    print("Delivered {} {}s for total of ${}".format(
+        count, melon, amount))
+the_file.close()
+
+
+print("Day 3")
+the_file = open("um-deliveries-20140521.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[0]
+    amount = words[0]
+
+    print("Delivered {} {}s for total of ${}".format(
+        count, melon, amount))
+the_file.close()
