10ebde9d14578068f6e77edab182621572d62ec2
Jennifer Park <jiyoungjenniferpark@gmail.com>
2021-03-21 23:13:22 +0000

    list unpack

--- produce_summary.py
+++ produce_summary.py
@@ -1,23 +1,19 @@
-def print_report(file_name):
+def print_report(day, file_name):
+    print("Day", day)
     the_file = open(file_name) #open Day's file
     for line in the_file: #go through every line in file
         line = line.rstrip() 
         words = line.split('|')
 
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+        melon, count, amount = words
 
         print("Delivered {} {}s for total of ${}".format(
             count, melon, amount))
     the_file.close()
 
-print("Day 1") #for day 1 
-print_report(file_name = "um-deliveries-20140519.txt")
+print_report(1, "um-deliveries-20140519.txt")
 
-print("Day 2")
-print_report(file_name = "um-deliveries-20140520.txt")
+print_report(2, "um-deliveries-20140520.txt")
 
-print("Day 3")
-print_report(file_name = "um-deliveries-20140521.txt")
+print_report(3, "um-deliveries-20140521.txt")
 
